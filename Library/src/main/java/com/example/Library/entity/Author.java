24bf7b4a0fee845fd8eaa5ba8a10c133486d42b6
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-06-13 17:59:29 -0400

    Initialize Author class

---
+++ Library/src/main/java/com/example/Library/entity/Author.java
@@ -0,0 +1,29 @@
+package com.example.Library.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<AuthorBook> authorBook = new ArrayList<>();
+
+
+}
