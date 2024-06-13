a14e002fe20eca8ee7e60f76f663ae60e55a1497
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-06-13 18:00:25 -0400

    Initialize Book class

---
+++ Library/src/main/java/com/example/Library/entity/Book.java
@@ -0,0 +1,27 @@
+package com.example.Library.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(unique = true, nullable = false)
+    private String title;
+
+    @OneToMany(mappedBy = "book", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<AuthorBook> authorBook = new ArrayList<>();
+}
