8053fc50778691c95a6734bd3b42728e8dd7c480
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-06-13 23:23:53 -0400

    Add more methods in book service

--- Library/src/main/java/com/example/Library/service/BookService.java
+++ Library/src/main/java/com/example/Library/service/BookService.java
@@ -4,10 +4,15 @@ import com.example.Library.entity.Book;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public interface BookService {
     List<Book> findAllBooks();
+    Optional<Book> findBookById(int id);
+    Book createBook(Book book, List<String> authors);
+    Book updateBook(int id, Book book, List<String> authors);
+    void deleteBook(int id);
     List<Book> findBooksByTitle(String title);
     List<Book> findBooksByAuthorName(String authorName);
 }
