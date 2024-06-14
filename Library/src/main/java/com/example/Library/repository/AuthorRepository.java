44dd84a728888531beb91f7a6d7c55673aaffa85
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-06-13 22:02:45 -0400

    Add customized query into author jpa repo

--- Library/src/main/java/com/example/Library/repository/AuthorRepository.java
+++ Library/src/main/java/com/example/Library/repository/AuthorRepository.java
@@ -2,8 +2,13 @@ package com.example.Library.repository;
 
 import com.example.Library.entity.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface AuthorRepository extends JpaRepository<Author, Integer> {
+    @Query("SELECT a FROM Author a WHERE a.name = ?1")
+    List<Author> findByName(String name);
 }
