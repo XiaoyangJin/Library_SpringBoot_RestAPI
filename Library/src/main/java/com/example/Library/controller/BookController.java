4287e30e8fee46e5be18bc52b6d7e1d9b9ed722b
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-06-14 00:40:04 -0400

    Finish all methods impl in Controller

--- Library/src/main/java/com/example/Library/controller/BookController.java
+++ Library/src/main/java/com/example/Library/controller/BookController.java
@@ -3,10 +3,7 @@ package com.example.Library.controller;
 import com.example.Library.entity.Book;
 import com.example.Library.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -29,5 +26,30 @@ public class BookController {
     @GetMapping("/{id}")
     public Optional<Book> getBookById(@PathVariable int id) {
         return bookService.findBookById(id);
+    }
+
+    @PostMapping
+    public Book createBook(@RequestBody Book book, @RequestParam List<String> authors) {
+        return bookService.createBook(book, authors);
+    }
+
+    @PutMapping("/{id}")
+    public Book updateBook(@PathVariable int id, @RequestBody Book book, @RequestParam List<String> authors) {
+        return bookService.updateBook(id, book, authors);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteBook(@PathVariable int id) {
+        bookService.deleteBook(id);
+    }
+
+    @GetMapping("/search")
+    public List<Book> getBooksByTitle(@RequestParam String title) {
+        return bookService.findBooksByTitle(title);
+    }
+
+    @GetMapping("/searchByAuthor")
+    public List<Book> getBooksByAuthorName(@RequestParam String authorName) {
+        return bookService.findBooksByAuthorName(authorName);
     }
 }
