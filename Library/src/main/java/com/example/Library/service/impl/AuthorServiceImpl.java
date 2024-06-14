1a5e6bf674381fc85fb31dd788948cf6a86afd3e
XiaoyangJin <jin.xiaoya@northeastern.edu>
2024-06-13 22:02:58 -0400

    Create AuthorServiceImpl.java

---
+++ Library/src/main/java/com/example/Library/service/impl/AuthorServiceImpl.java
@@ -0,0 +1,30 @@
+package com.example.Library.service.impl;
+
+import com.example.Library.entity.Author;
+import com.example.Library.repository.AuthorBookRepository;
+import com.example.Library.repository.AuthorRepository;
+import com.example.Library.service.AuthorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AuthorServiceImpl implements AuthorService {
+
+    private final AuthorRepository authorRepository;
+    @Autowired
+    public AuthorServiceImpl(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @Override
+    public List<Author> findAllAuthors() {
+        return authorRepository.findAll();
+    }
+
+    @Override
+    public List<Author> findAuthorsByName(String name) {
+        return authorRepository.findByName(name);
+    }
+}
